a3843e3825b5b1e778858cc4d83dd9c704e7e2db
marcosledesma <marcosledesmapicazo@gmail.com>
2021-01-08 16:04:21 +0100

    Initial Commit

---
+++ Ejercicio02-Agenda de Contactos/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Ejercicio02-Agenda de Contactos
+//
+//  Created by Marcos Ledesma on 08/01/2021.
+//  Copyright © 2021 Marcos Ledesma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
